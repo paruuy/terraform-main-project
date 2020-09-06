9dc21319c329ce10335a75382e1e7a44e32b5848
Pablo Rodriguez <pablorodriguez@MBP-de-Pablo.lan>
2020-09-06 10:45:52 +0100

    changed module's variables (hardcoded data)

--- terrafile.tf
+++ terrafile.tf
@@ -5,8 +5,9 @@ provider "aws" {
 
 module "ec2" {
   source        = "git@github.com:paruuy/terraform-remote-module.git?ref=v0.2" # Repo of terrafom module, with tag v0.1
-  app_name      = each.value.app_name
-  instance_type = each.value.instance_type  
+  app_name                = "turma3"
+  instance_type           = "t3.micro"
+  count                   = 2
 }
 
 output "ip_address_ec2" {
